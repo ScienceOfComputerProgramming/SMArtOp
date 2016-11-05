bdb0c55c7d8e50b96195d50894fb41daaabbb3a0
Antonela <tommantonela@gmail.com>
2016-11-05 14:49:32 -0300

    Added JPPF V5 and Maven support. Added new Matrix CVS format.

--- smartop/src/matrix/factory/FactoryMatrixSparse.java
+++ smartop/src/matrix/factory/FactoryMatrixSparse.java
@@ -54,10 +54,10 @@ public class FactoryMatrixSparse implements FactoryMatrix {
 			identity.setValue(i, i, 1);
 		return identity;
 	}
-	
+
 	@Override
 	public Matrix createMatrix(String pathToCopy) {
-		
+
 		BufferedReader io;
 		Matrix loaded = null;
 		try {
@@ -65,20 +65,31 @@ public class FactoryMatrixSparse implements FactoryMatrix {
 			String l = io.readLine();
 			String [] sizes = l.split(",");
 			loaded = new MatrixSparse(Integer.parseInt(sizes[0]),Integer.parseInt(sizes[1]));
-			l = io.readLine();
-			int i = 0;
-			while(l!=null){
-				String [] row = l.split(",");
-				for(int j=0;j<row.length;j++)
-					loaded.setValue(i, j, Float.parseFloat(row[j]));
+			if(sizes.length == 2){ //dense csv format
 				l = io.readLine();
-				i++;
+				int i = 0;
+				while(l!=null){
+					String [] row = l.split(",");
+					for(int j=0;j<row.length;j++)
+						loaded.setValue(i, j, Float.parseFloat(row[j]));
+					l = io.readLine();
+					i++;
+				}
+			}else{
+				if(sizes.length == 3){ //sparse csv format
+					l = io.readLine();
+					while(l!=null){
+						String [] element = l.split(",");
+						loaded.setValue(Integer.parseInt(element[0]),Integer.parseInt(element[1]), Float.parseFloat(element[2]));
+						l = io.readLine();
+					}
+				}
 			}
 			io.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return loaded;
 	}
 }
--- smartop/src/matrix/factory/FactoryMatrixThreshold.java
+++ smartop/src/matrix/factory/FactoryMatrixThreshold.java
@@ -34,14 +34,14 @@ import matrix.matrixImpl.MatrixThreshold;
  *
  */
 public class FactoryMatrixThreshold implements FactoryMatrix {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 463816389340234059L;
-	
+
 	private float threshold;
-	
+
 	/**
 	 * Creates the factory with a threshold defined in {@link Configuration}.
 	 */
@@ -76,7 +76,7 @@ public class FactoryMatrixThreshold implements FactoryMatrix {
 
 	@Override
 	public Matrix createMatrix(String pathToCopy) {
-		
+
 		BufferedReader io;
 		Matrix loaded = null;
 		try {
@@ -84,21 +84,32 @@ public class FactoryMatrixThreshold implements FactoryMatrix {
 			String l = io.readLine();
 			String [] sizes = l.split(",");
 			loaded = new MatrixThreshold(Integer.parseInt(sizes[0]),Integer.parseInt(sizes[1]),threshold);
-			l = io.readLine();
-			int i = 0;
-			while(l!=null){
-				String [] row = l.split(",");
-				for(int j=0;j<row.length;j++)
-					loaded.setValue(i, j, Float.parseFloat(row[j]));
+			if(sizes.length == 2){ //dense csv format
 				l = io.readLine();
-				i++;
+				int i = 0;
+				while(l!=null){
+					String [] row = l.split(",");
+					for(int j=0;j<row.length;j++)
+						loaded.setValue(i, j, Float.parseFloat(row[j]));
+					l = io.readLine();
+					i++;
+				}
+			}else{
+				if(sizes.length == 3){ //sparse csv format
+					l = io.readLine();
+					while(l!=null){
+						String [] element = l.split(",");
+						loaded.setValue(Integer.parseInt(element[0]),Integer.parseInt(element[1]), Float.parseFloat(element[2]));
+						l = io.readLine();
+					}
+				}
 			}
 			io.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return loaded;
 	}
-	
+
 }
